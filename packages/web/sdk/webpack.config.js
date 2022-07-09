23b1740d34b3f466f7a13406c64e602853369259
Michael Riezler <michael@riezler.co>
2022-07-09 11:23:24 +0200

    update(packages/web/sdk): move axios interceptor into different output

--- packages/web/sdk/webpack.config.js
+++ packages/web/sdk/webpack.config.js
@@ -3,6 +3,11 @@ let createConfig = require('../../../webpack.config')
 module.exports = function ({ browser }, argv) {
 	let config = createConfig(argv.mode || 'production')
 
+	config.entry = {
+		main: './src/main.ts',
+		interceptor: './src/interceptor.ts',
+	}
+
 	if (browser) {
 		config.output = {
 			library: 'VulpoAuth',
