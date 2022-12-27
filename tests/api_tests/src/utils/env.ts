2ff29ef71cd7f38a802f8e5669846251b14065a0
Michael Riezler <michael@riezler.co>
2022-12-27 16:56:28 +0100

    fix: api tests

--- tests/api_tests/src/utils/env.ts
+++ tests/api_tests/src/utils/env.ts
@@ -1,4 +1,4 @@
 import { project } from '@seeds/data/projects'
 
 export let PROJECT_ID = process.env.PROJECT_ID ?? project.id
-export let SERVER_URL = process.env.SERVER_URL ?? "http://localhost:8000"
+export let SERVER_URL = process.env.SERVER_URL ?? "http://127.0.0.1:8000"
